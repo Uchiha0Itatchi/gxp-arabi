4319182763f2b308f57fc404e6155a94029786dd
Uchiha0Itatchi <95933910+Uchiha0Itatchi@users.noreply.github.com>
2021-12-10 22:46:01 +0200

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+function openMenu() {
+    document.getElementById("fas").style.display = "none";
+    document.getElementById("far").style.display = "block";
+    document.getElementById("menu-icon").style.marginTop = "5px";
+    document.getElementById("marn").style.display = "none";
+
+  }
+  
+  function closeMenu() {
+    document.getElementById("far").style.display = "none";
+    document.getElementById("fas").style.display = "block";
+    document.getElementById("marn").style.display = "block";
+
+  }
+
---
+++ js/preload.js
@@ -0,0 +1,3 @@
+
+
+ $(window).load(function(){$(".preload").fadeOut(),$("#preloader").delay(350).fadeOut("slow"),$("body").delay(350).css({overflow:"visible"})});
